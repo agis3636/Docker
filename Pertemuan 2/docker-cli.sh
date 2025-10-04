ce6a4baf57262ccc1465f4d9aa15c2c98c8a587b
Agis <asyelom@gmail.com>
2025-10-04 17:18:49 +0700

    Update docker-cli.sh

--- Pertemuan 2/docker-cli.sh
+++ Pertemuan 2/docker-cli.sh
@@ -2,16 +2,18 @@ docker images ls
 docker image pull redis:latest
 docker images ls
 coba diulang lagi nanti dia tidak akan download ulang
-
+---
 docker container create --name contohredis1 redis:latest
 docker container start contohredis1
-
+---
 container log :
+
 docker container logs contohredis1
 docker container logs -f contohredis1
 exit = ctrl+c
-
+---
 container exec :
+
 docker container exec -i -t contohredis1 /bin/bash
 root@343535252 > ls
 root@343535252 > cd /
@@ -21,8 +23,9 @@ root@343535252 > redis-cli
 127.0.0.1:6379 > get name
 127.0.0.1:6379 > exit
 root@343535252 > exit
-
+---
 container port, melakukan port forwarding dengan nginx + exec :
+
 docker image pull nginx:latest
 docker container create --name contohnginx1 --publish 8080:80 nginx:latest
 docker container ls -a
