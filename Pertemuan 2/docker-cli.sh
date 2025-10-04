fd07cad5c6673817d68011d141ca778fe946a811
Agis <asyelom@gmail.com>
2025-10-04 16:27:57 +0700

    Update docker-cli.sh

--- Pertemuan 2/docker-cli.sh
+++ Pertemuan 2/docker-cli.sh
@@ -4,3 +4,11 @@ docker images ls
 coba diulang lagi nanti dia tidak akan download ulang
 
 docker container create --name contohredis1 redis:latest
+docker container start contohredis1
+
+container log :
+docker container logs contohredis1
+docker container logs -f contohredis1
+exit = ctrl+c
+
+container exec :
