8c4b8343ca973748d59c37dde25db0d3e5df9c8b
Agis <asyelom@gmail.com>
2025-11-08 23:53:38 +0700

    Update dockerfile.sh

--- dockerfile.sh
+++ dockerfile.sh
@@ -19,3 +19,20 @@ edit file Dockerfile di dalam directory
 build docker di terminal
   docker build -t agis3636/run run
   docker build -t agis3636/run run --progress=plain --no-cache
+  docker image ls | grep agis3636
+
+  # CMD INSTRUCTION
+edit file Dockerfile di dalam directory
+  FROM alpine:3
+
+  RUN mkdir hello
+  Run echo "hello world" > "hello/world.txt"
+  
+  CMD cat "hello/world.txt"
+
+build docker di terminal
+  docker build -t agis3636/command command
+  docker image inspect agis3636/command
+  docker container create --name command agis3636/command
+  docker container start command
+  docker container logs command
