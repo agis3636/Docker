ad5f53a84087fa36542cd03cb426a0750590a3b9
Agis <asyelom@gmail.com>
2025-11-08 22:56:20 +0700

    Create dockerfile.sh

---
+++ dockerfile.sh
@@ -0,0 +1,8 @@
+# FROM INSTRUCTION
+buat dile Dockerfile ke dalam directory
+  FROM alpine:3
+
+build docker di terminal
+  docker build -t agis3636/namaimages directory
+  docker image ls
+  docker images
