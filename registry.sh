bdacf628a6020e16b465079bea96b5d3adf6f2b1
Agis <asyelom@gmail.com>
2025-12-28 10:07:41 +0700

    Update registry.sh

--- registry.sh
+++ registry.sh
@@ -1,1 +1,21 @@
-
+DOCKER HUB REGISTRY
+------------------------------------------------------------------------------------------
+  Token Belajar:
+    dckr_pat_uaATiKsxAe64dCaFqrYl6TN4j3U
+------------------------------------------------------------------------------------------
+  Docker Login:
+    docker login -u agis3636
+    
+    i Info → A Personal Access Token (PAT) can be used instead.
+             To create a PAT, visit https://app.docker.com/settings
+                        
+    Password: 
+    
+    WARNING! Your credentials are stored unencrypted in '/home/agis/.docker/config.json'.
+    Configure a credential helper to remove this warning. See
+    https://docs.docker.com/go/credential-store/
+    
+    Login Succeeded
+------------------------------------------------------------------------------------------
+  Docker Push:
+    docker push agis3636/image:v1.0
