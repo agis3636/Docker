052678119dd10853c1a568aa9cbe275df1191537
Agis <asyelom@gmail.com>
2026-01-04 10:01:22 +0700

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -11,6 +11,33 @@ buat file dengan nama "docker-compose.yaml"
 
 ---
 
-untuk menjalankan Container, harus masuk ke dalam directory yang menyimpan file "docker-compose.yaml"
+untuk membuat Container, harus masuk ke dalam directory yang menyimpan file "docker-compose.yaml"
 
     docker compose create
+    docker container ls -a
+
+---
+
+mau bikin lagi ?
+
+    services:
+        nginx-example:
+            container_name: nginx-example
+            image: nginx:latest
+        nginx-example2:
+            container_name: nginx-example2
+            image: nginx:latest
+
+---
+
+untuk membuat Container, harus masuk ke dalam directory yang menyimpan file "docker-compose.yaml"
+
+    docker compose create
+    docker container ls -a
+
+---
+
+jalankan container
+
+    docker compose start
+    docker container ls
